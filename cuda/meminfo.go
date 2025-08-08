824fc69727e49c9b643950f8bf0817c7f166abab
vunv <vunv@coccoc.com>
2025-08-08 14:29:07 +0700

    nothing...

--- cuda/meminfo.go
+++ cuda/meminfo.go
@@ -1,9 +1,52 @@
 package cuda
 
+/*
+#cgo LDFLAGS: -L. -lgpudevice -lcudart -Wl,-rpath=.
+#include <cuda_runtime.h>
+char* checkCuda(cudaError_t err);
+*/
+import "C"
 import (
 	"fmt"
 	"log"
 )
+
+func RealCudaGetDeviceCount(count *int) error {
+	var cCount C.int
+	errStr := C.checkCuda(C.cudaGetDeviceCount(&cCount))
+	if errStr != nil {
+		return fmt.Errorf("cuda get device count error: %s", C.GoString(errStr))
+	}
+	*count = int(cCount)
+	return nil
+}
+
+func SetCudaDevice(deviceID int) error {
+	errStr := C.checkCuda(C.cudaSetDevice(C.int(deviceID)))
+	if errStr != nil {
+		return fmt.Errorf("cudaSetDevice failed: %s", C.GoString(errStr))
+	}
+	return nil
+}
+
+func CudaGetMemInfo(deviceID int) (*MemInfo, error) {
+	if err := SetCudaDevice(deviceID); err != nil {
+		return nil, err
+	}
+
+	var free C.size_t
+	var total C.size_t
+
+	errStr := C.checkCuda(C.cudaMemGetInfo(&free, &total))
+	if errStr != nil {
+		return nil, fmt.Errorf("cudaMemGetInfo failed: %s", C.GoString(errStr))
+	}
+	return &MemInfo{
+		DeviceID: deviceID,
+		Free:     int64(free),
+		Total:    int64(total),
+	}, nil
+}
 
 type MemInfo struct {
 	DeviceID int
--- script/test_cu_alloc.c
+++ script/test_cu_alloc.c
@@ -1,4 +1,5 @@
 #include <cuda.h>
+#include <cuda_runtime.h>
 #include <stdio.h>
 
 int main() {
